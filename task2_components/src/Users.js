aecae5efda46ae95c2212ec2cff44e497c315e76
kodehodekim <kim.kodehode@gmail.com>
2022-12-12 13:47:47 +0100

    .

---
+++ task2_components/src/Users.js
@@ -0,0 +1,22 @@
+//prettier-ignore
+
+const UserInfo = [
+  {
+    name: "Endre",
+    age: 32,
+  },
+  {
+    name: "Glenn",
+    age: 122,
+  },
+  {
+    name: "Joe",
+    age: 45,
+  },
+  { 
+    name: "Katrine", 
+    age: 12 
+  }
+];
+
+export default UserInfo;
