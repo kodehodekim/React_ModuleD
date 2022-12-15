f76878f433751b60b37a039d9d664b99e8a6194d
kodehodekim <kim.kodehode@gmail.com>
2022-12-15 10:23:31 +0100

    .

---
+++ task2_components/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div className="Header">
+        <h1>THIS IS THE HEADER ISNT IT NICE</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ task2_components/src/components/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <main>
+      <h2>This is the main content</h2>
+      <h4>Very nice isnt it.</h4>
+      <button>Click Me</button>
+    </main>
+  );
+}
+
+export default Main;
